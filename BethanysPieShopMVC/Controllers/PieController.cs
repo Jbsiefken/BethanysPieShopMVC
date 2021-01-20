0226f29c1bd1e46795948956e7f9656ae76e00c7
Josh Siefken <joshbsiefken@gmail.com>
2021-01-20 17:37:26 -0600

    initial commit

---
+++ BethanysPieShopMVC/Controllers/PieController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShopMVC.Models;
+using BethanysPieShopMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShopMVC.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        //constructor: controller is called with reference to a pieRepo and a categoryRepo
+        //and the constructor assigns them to the properties
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        //This is the action method. the return calls the View method with AllPies as an argument
+        public ViewResult List()
+        {
+            PiesListViewModel piesListViewModel = new PiesListViewModel();
+            piesListViewModel.Pies = _pieRepository.AllPies;
+            piesListViewModel.CurrentCategory = "Cheese cakes";
+
+            return View(piesListViewModel);
+        }
+    }
+}
---
+++ BethanysPieShopMVC/Models/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopMVC.Models
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        //these properties establish which models get tables
+        public DbSet<Pie> Pies { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
